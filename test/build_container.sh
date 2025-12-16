b936247ee9813130d73cdbc0a82eda9b036ab705
Phuc Vinh <81987648+pvinh-spike@users.noreply.github.com>
2025-12-15 16:48:09 -0800

    Remove double quote

--- test/build_container.sh
+++ test/build_container.sh
@@ -6,7 +6,7 @@ function build_container() {
   local distro=$1
   docker build \
     --progress=plain \
-    --build-arg=BASE_IMAGE="${distro_to_image["$distro"]}" \
+    --build-arg=BASE_IMAGE="${distro_to_image[$distro]}" \
     --build-arg=ENV_DISTRO="$distro" \
     --build-arg=PKG_VERSION="$PKG_VERSION" \
     --build-arg=JF_USERNAME="$JF_USERNAME" \
